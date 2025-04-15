e5807895e18ab319c32f0d076843c13d6d834134
Dave Lewis <siu06dl@gmail.com>
2025-04-15 18:27:28 +0100

    feat: initial SQL Server controller

---
+++ Flashcards/Controllers/SqlServerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace Flashcards.Controllers;
+
+internal class SqlServerController
+{
+    private readonly string _connectionString = ConfigurationManager.ConnectionStrings["Flashcards"].ConnectionString;
+    
+    private SqlConnection GetConnection() => 
+        new(_connectionString);
+
+    internal async Task RunExample()
+    {
+        try
+        {
+            await using var connection = GetConnection();
+            Console.WriteLine("\nQuery data example:");
+            Console.WriteLine("=========================================\n");
+
+            await connection.OpenAsync();
+
+            var sql = "SELECT name, collation_name FROM sys.databases";
+            
+            await using var command = new SqlCommand(sql, connection);
+            await using var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+            }
+        }
+        //catch (SqlException e) when (e.Number == /* specific error number */)
+        catch (SqlException e)
+        {
+            Console.WriteLine($"SQL Error: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+}
--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,40 +1,5 @@
-﻿using System.Configuration;
-using Microsoft.Data.SqlClient;
+﻿using Flashcards.Controllers;
 
-var connectionString  = ConfigurationManager.ConnectionStrings["Flashcards"].ConnectionString;
+var database = new SqlServerController();
 
-/*
-var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
-    .FilePath;
-    
-Console.WriteLine(config);
-*/
-
-
-
-try
-{
-    await using var connection = new SqlConnection(connectionString);
-    Console.WriteLine("\nQuery data example:");
-    Console.WriteLine("=========================================\n");
-
-    await connection.OpenAsync();
-
-    var sql = "SELECT name, collation_name FROM sys.databases";
-    await using var command = new SqlCommand(sql, connection);
-    await using var reader = await command.ExecuteReaderAsync();
-
-    while (await reader.ReadAsync())
-    {
-        Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
-    }
-}
-//catch (SqlException e) when (e.Number == /* specific error number */)
-catch (SqlException e)
-{
-    Console.WriteLine($"SQL Error: {e.Message}");
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.ToString());
-}
+await database.RunExample();
