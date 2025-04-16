188ba38daa26c814384a14aeb32631ae76b29ec9
Dave Lewis <siu06dl@gmail.com>
2025-04-16 18:50:11 +0100

    feat: initial menu

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Flashcards.Controllers;
+using Flashcards.View;
 
 var database = new SqlServerController();
-
 await database.Initialize();
+
+return new MainMenu(database).Run();
---
+++ Flashcards/View/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using Flashcards.Controllers;
+using Spectre.Console;
+
+namespace Flashcards.View;
+
+internal class MainMenu(SqlServerController database)
+{
+    internal int Run()
+    {
+        var optionList = new List<string> { "Exit" };
+        
+        while (true)
+        {
+            AnsiConsole.Clear();
+
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What would you like to do next?")
+                    .AddChoices(optionList));
+
+            switch (choice)
+            {
+                case "Exit":
+                    return 0;
+            }
+        }
+    }
+}
