95c602ed2730d0101ed57049898444c9d084d6f4
Dave Lewis <siu06dl@gmail.com>
2025-04-17 12:05:28 +0100

    feat: card validation

--- Flashcards/Model/Card.cs
+++ Flashcards/Model/Card.cs
@@ -2,6 +2,27 @@
 
 internal class Card
 {
+    private const int MaxStringLength = 255;
+    
     internal required string Front { get; set; }
     internal required string Back { get; set; }
+
+    internal static bool IsValidString(string proposed, string type, out string error)
+    {
+        error = string.Empty;
+
+        if (string.IsNullOrEmpty(proposed))
+        {
+            error = $"{type} must not be empty";
+            return false;
+        }
+
+        if (proposed.Length  > MaxStringLength)
+        {
+            error = $"{type} must be less than [orange3]{MaxStringLength}[/] characters!";
+            return false;
+        }
+        
+        return true;
+    }
 }
