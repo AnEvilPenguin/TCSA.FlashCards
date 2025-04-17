0bdd4c3e7c2757a4695eb4716329b0e39453d56e
Dave Lewis <siu06dl@gmail.com>
2025-04-17 10:20:00 +0100

    fix: handle server offline error on creation

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,7 +1,18 @@
 ﻿using Flashcards.Controllers;
 using Flashcards.View;
+using Microsoft.Data.SqlClient;
+using Spectre.Console;
 
 var database = new SqlServerController();
-await database.Initialize();
+
+try
+{
+    await database.Initialize();
+}
+catch (SqlException sqlEx)
+{
+    AnsiConsole.Markup($"[red]CRITICAL: {sqlEx.Message}[/]");
+    return 1;
+}
 
 return await new MainMenu(database).DisplayMenu();
